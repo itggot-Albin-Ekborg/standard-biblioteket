f15235cd7374d8f0e0da4f3bb27f1a1745be6e51
itggot-Albin-Ekborg <albin.ekborg@itggot.se>
2018-04-26 15:03:56 +0200

    Added power.rb

---
+++ lib/power.rb
@@ -0,0 +1,15 @@
+# Public: Takes two numbers and returns the base to the power of the exponent.
+#
+# num1 - The base number.
+# num2 - The exponent number.
+#
+# Examples
+#
+#   power(2, 3)
+#   # => 8
+#
+# Returns the base to the power of the exponent.
+
+def power(num1, num2)
+    return num1**num2
+end
